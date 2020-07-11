5beec7a3617fba7f93bef9e1d9ecbab13cca917b
weepingfish <weepingfish@outlook.com>
2020-07-10 22:05:27 -0700

    Site updated: 2020-07-10 22:05:26

--- update.sh
+++ update.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
 while true; do
+    git add -A && git commit -m -
     git fetch
     DIFF=$(git rev-list HEAD...origin/master --count)
 
 	if [ $DIFF -gt 0 ]; then
 		echo "Need to pull"
         git pull
+        git push
         hexo clean
         hexo g
         hexo d
